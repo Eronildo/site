5a7e7f3e6b494bdd0a812e00dde271f1ee01c5ad
Eronildo <eronildoj@gmail.com>
2019-09-19 14:00:15 -0300

    Initial Site

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:site/src/widgets/wave/wave.dart';
+import 'package:site/src/widgets/wave/config.dart';
+import 'package:site/theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.theme(),
+      home: Scaffold(
+        body: Container(
+          child: HomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  AnimationController _baseAnimation;
+  Animation<double> _wavesAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _baseAnimation = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 3000),
+    )..forward();
+    _wavesAnimation = CurvedAnimation(
+      parent: _baseAnimation,
+      curve: const Interval(0.3, 1.0),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: SizedBox(
+            height: 500.0,
+          ),
+        ),
+        Flexible(
+          child: AnimatedBuilder(
+            animation: _wavesAnimation,
+            builder: (context, child) {
+              return Transform.translate(
+                offset: Offset(0.0, 250 - (Curves.fastLinearToSlowEaseIn.transform(_wavesAnimation.value) * 250)),
+                child: child,
+              );
+            },
+            child: WaveWidget(
+              config: WaveConfigCustom(
+                gradients: [
+                  [AppTheme.colorBackgroundLight, Colors.blue],
+                  [Colors.blue[800], Colors.blue[300]],
+                  [Colors.blue[900], AppTheme.colorBackgroundLight],
+                  [AppTheme.colorDefaultGradientOne, AppTheme.colorDefaultGradientTwo],
+                ],
+                durations: [
+                  35000,
+                  19440,
+                  10800,
+                  6000,
+                ],
+                heightPercentages: [
+                  0.20,
+                  0.23,
+                  0.25,
+                  0.30,
+                ],
+                gradientBegin: Alignment.bottomLeft,
+                gradientEnd: Alignment.topRight,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
---
+++ lib/theme.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  const AppTheme._();
+
+  static const fontFamilyProductSans = 'Product Sans';
+  static const fontFamilyFiraCode = 'Fira Code';
+  static const fontFamilyDefault = fontFamilyProductSans;
+
+  static const fontWeightExtraLight = FontWeight.w200;
+  static const fontWeightLight = FontWeight.w300;
+  static const fontWeightNormal = FontWeight.w400;
+  static const fontWeightMedium = FontWeight.w500;
+  static const fontWeightSemiBold = FontWeight.w600;
+  static const fontWeightBold = FontWeight.w700;
+
+  static const colorTextPrimary = Colors.white;
+  static const colorTextSecondary = Color(0xFFB7B9C0);
+  static const colorTextTernary = Color(0xFF6E7380);
+
+  static const colorBackgroundLight = Color(0xFF23222D);
+  static const colorBackgroundDark = Color(0xFF15161E);
+
+  static const colorRed = Color(0xFFF22D55);
+  static const colorGreen = Color(0xFF29FFCA);
+
+  static const colorDefaultGradientOne = Colors.cyan;
+  static const colorDefaultGradientTwo = colorGreen;
+  static final gradientDefault = LinearGradient(
+    colors: [colorDefaultGradientOne, colorDefaultGradientTwo],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+  );
+
+  static const animationCurveDefault = Curves.fastLinearToSlowEaseIn;
+
+  static ThemeData theme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      backgroundColor: colorBackgroundDark,
+      scaffoldBackgroundColor: colorBackgroundLight,
+      primaryColor: colorBackgroundDark,
+      accentColor: colorGreen,
+      toggleableActiveColor: colorGreen,
+      canvasColor: colorBackgroundLight,
+      cardColor: colorBackgroundDark,
+      fontFamily: fontFamilyDefault,
+      buttonTheme: ButtonThemeData(
+        buttonColor: Colors.black,
+        disabledColor: Colors.black,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        fillColor: Colors.white,
+        filled: true,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey,
+            width: 2,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey,
+            width: 2,
+          ),
+        ),
+        isDense: true,
+        alignLabelWithHint: false,
+      ),
+    );
+  }
+}
